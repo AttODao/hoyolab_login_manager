c7cc9e5f9a43a2fc9facff6cbb98f711d383907c
AttODao <atsuatat@gmail.com>
2024-10-06 16:59:30 +0900

    zzz commands

--- src/commands/choices.rs
+++ src/commands/choices.rs
@@ -4,12 +4,14 @@ use api::types::Game;
 pub enum GameChoice {
   Genshin,
   Starrail,
+  Zzz,
 }
 impl GameChoice {
   pub fn to_game(&self) -> Game {
     match self {
       Self::Genshin => Game::Genshin,
       Self::Starrail => Game::Starrail,
+      Self::Zzz => Game::Zzz,
     }
   }
 }
@@ -22,8 +24,8 @@ pub enum SetChoice {
 impl SetChoice {
   pub fn to_bool(&self) -> bool {
     match self {
-        SetChoice::Enable => true,
-        SetChoice::Disable => false,
+      SetChoice::Enable => true,
+      SetChoice::Disable => false,
     }
   }
 }
